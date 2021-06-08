73329018c7ba566c00680c53d8610e9f88537ca1
Renek1992 <rene.kesenheimer@icloud.com>
2021-06-08 16:56:11 +0100

    added circleci config

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 # Step 1:
-dockerpath="renekesenheimer/udacitymlapi"
+dockerpath="renekesenheimer/udacitymlapi:latest"
 
 # Step 2
-kubectl run mlmicroserviceapi \
+kubectl run udacitymlapi \
                 --image=$dockerpath \
-                --port=80 \
-                --labels app=mlmicroserviceapi
+                --port=80
 
 # Step 3:
 sleep 5
 kubectl get pods
 
 # Step 4:
-kubectl port-forward mlmicroserviceapi 8000:80
+kubectl port-forward udacitymlapi 8000:80
 
